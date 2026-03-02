5b425cae0ac0c868cb99bd662f0a8d6a92a0e76f
Renato Diniz <dinizre@hotmail.com>
2026-03-02 16:11:29 -0300

    feat(core): add enterprise enums for asset radar

---
+++ src/Auction.AssetRadar.Core/AssetRadarEnums.cs
@@ -0,0 +1,26 @@
+namespace Auction.AssetRadar;
+
+public enum PropertyType
+{
+    Apartamento = 0,
+    Casa = 1,
+    Terreno = 2,
+    Predio = 3,
+    Sala = 4,
+    Comercial = 5,
+    Galpao = 6,
+    Loja = 7,
+    Sobrado = 8,
+    Gleba = 9,
+    ImovelRural = 10,
+    Outros = 11
+}
+
+public enum SaleModality
+{
+    LeilaoSfiEditalUnico = 0,
+    VendaDiretaOnline = 1,
+    LicitacaoAberta = 2,
+    VendaOnline = 3,
+    Unknown = 4
+}
